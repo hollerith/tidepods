e5aaa539e676a2a7325b68b18dbca8de081f35e1
N.Mous <nmous@oznz.me>
2021-07-12 19:26:41 +0100

    adding unit tests to test service

--- hello/tests.py
+++ hello/tests.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AnonymousUser, User
 from django.test import TestCase, RequestFactory
 
 from .views import index
-
+from .specs import TrackingRequestXML
 
 class SimpleTest(TestCase):
     def setUp(self):
@@ -17,3 +17,41 @@ class SimpleTest(TestCase):
         # Test my_view() as if it were deployed at /customer/details
         response = index(request)
         self.assertEqual(response.status_code, 200)
+
+    def test_weather(self):
+        """
+        Check the weather 
+        """
+        import requests
+
+        url="https://wsbeta.fedex.com:443/web-services"
+        #headers = {'content-type': 'application/soap+xml'}
+        headers = {'content-type': 'text/xml'}
+        body = """<?xml version="1.0" encoding="UTF-8"?>
+         <SOAP-ENV:Envelope xmlns:ns0="http://ws.cdyne.com/WeatherWS/" xmlns:ns1="http://schemas.xmlsoap.org/soap/envelope/" 
+            xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/">
+            <SOAP-ENV:Header/>
+              <ns1:Body><ns0:GetWeatherInformation/></ns1:Body>
+         </SOAP-ENV:Envelope>"""
+
+        response = requests.post(url,data=body,headers=headers)
+        print(response.content)
+
+    def test_tracking(self):
+        """
+        Check the service 
+        """
+        import requests
+
+        url="https://wsbeta.fedex.com:443/web-services"
+        #headers = {'content-type': 'application/soap+xml'}
+        headers = {'content-type': 'text/xml'}
+        body = """<?xml version="1.0" encoding="UTF-8"?>
+         <SOAP-ENV:Envelope xmlns:ns0="http://ws.cdyne.com/WeatherWS/" xmlns:ns1="http://schemas.xmlsoap.org/soap/envelope/" 
+            xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/">
+            <SOAP-ENV:Header/>
+              <ns1:Body><ns0:GetWeatherInformation/></ns1:Body>
+         </SOAP-ENV:Envelope>"""
+
+        response = requests.post(url,data=body,headers=headers)
+        print(response.content)
